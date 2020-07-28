bdf198c4edc7b898b2cc2b8272d408f481245c36
yuchisong <yuchisong7@gmail.com>
2020-07-28 09:42:23 +0900

    Data insert

---
+++ src/main/java/com/example/portfolio/InsertActivity.java
@@ -0,0 +1,228 @@
+package com.example.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.UUID;
+
+public class InsertActivity extends AppCompatActivity {
+ EditText shopnameinput, addressinput, roadaddressinput, businesshourinput, mobileinput;
+ Button btninsert;
+
+
+
+
+    class InsertThread extends Thread{
+        public void run () {
+            try {
+                //upload 할 주소 만들기
+                URL url = new URL("http://192.168.0.45:8080/portfolio/insert");
+                //서버에게 넘겨줄 문자열 파라미터 생성
+              /*  String[] data = {shopnameinput.getText().toString().trim(),
+                        addressinput.getText().toString().trim(),
+                        roadaddressinput.getText().toString().trim(),
+                        businesshourinput.getText().toString().trim(),
+                        mobileinput.getText().toString().trim()};
+                Log.e("shopname", shopnameinput.getText().toString());
+                Log.e("address", addressinput.getText().toString());
+                Log.e("roadaddress", roadaddressinput.getText().toString());
+                Log.e("mobile", mobileinput.getText().toString());
+                Log.e("businesshour", businesshourinput.getText().toString());
+
+                String[] dataName = {"shopname", "address", "roadaddress", "businesshour", "mobile"};
+                //파라미터 전송에 필요한 변수를 생성
+                String lineEnd = "\r\n";
+                //파일 업로드를 할 때는 boundary 값이 있어야 함
+                //랜덤하게 생성하는 것을 권장
+                //String boundary  ="androidinsert";
+                String boundary = UUID.randomUUID().toString();
+               */
+                //업로드 옵션을 설정
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                con.setConnectTimeout(30000);
+                con.setUseCaches(false);
+                con.setDoInput(true);
+                con.setDoOutput(true);
+
+                String data = URLEncoder.encode("shopname", "UTF-8") + "=" +
+                        URLEncoder.encode(shopnameinput.getText().toString().trim(), "UTF-8");
+                data += "&" + URLEncoder.encode("address", "UTF-8") + "=" +
+                        URLEncoder.encode(addressinput.getText().toString().trim(), "UTF-8");
+                data += "&" + URLEncoder.encode("roadaddress", "UTF-8") + "=" +
+                        URLEncoder.encode(roadaddressinput.getText().toString().trim(), "UTF-8");
+                data += "&" + URLEncoder.encode("mobile", "UTF-8") + "=" +
+                        URLEncoder.encode(mobileinput.getText().toString().trim(), "UTF-8");
+                data += "&" + URLEncoder.encode("businesshour", "UTF-8") + "=" +
+                        URLEncoder.encode(businesshourinput.getText().toString().trim(), "UTF-8");
+                OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                //파일 업로드 옵션 설정
+                //con.setRequestProperty("ENCTYPE", "multipart/form-data");
+                //con.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                //문자열 파라미터를 전송
+               /* String delimiter = "--" + boundary + lineEnd;
+                StringBuffer postDataBuilder = new StringBuffer();
+
+                for (int i = 0; i < data.length; i = i + 1) {
+                    postDataBuilder.append(delimiter);
+                    postDataBuilder.append("Content-Disposition: form-data; name=\"" +
+                            dataName[i] + "\"" + lineEnd + lineEnd + data[i] + lineEnd);
+                }
+
+                 */
+                //파라미터 전송
+               // DataOutputStream ds = new DataOutputStream(con.getOutputStream());
+                //ds.write(postDataBuilder.toString().getBytes());
+
+                //업로드할 파일이 있는 경우에만 작성
+              /*  String fileName = "ball.png";
+                if(fileName != null){
+                    postDataBuilder.append(delimiter);
+                    postDataBuilder.append("Content-Disposition:form-data;name=\""+
+                            "pictureurl" + "\";filename=\"" + fileName + "\"")+
+                            lineEnd);
+                }*/
+
+              /*  //파일 업로드
+                if(fileName != null){
+                    ds.writeBytes(lineEnd);
+                    //파일 읽어오기 - id에 해당하는 파일을 raw 디렉토리에 복사
+                    InputStream fres = getResources().openRawResource(R.raw.ball);
+                    byte [] buffer = new byte[fres.available()];
+                    int length = -1;
+                    //파일의 내용을 ㅇ릭어서 읽은 내용이 있으면 그 내용을 ds에 기록
+                    while((length = fres.read(buffer))!=-1){
+                        ds.write(buffer, 0, length);
+                    }
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes("--"+boundary+"--"+lineEnd);
+                    fres.close();
+                }else{
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes("=="+boundary+"--"+lineEnd);
+                }
+                */
+                /*   ds.writeBytes(lineEnd);
+                    ds.writeBytes("=="+boundary+"--"+lineEnd);
+
+                ds.flush();
+                ds.close();
+                 */
+                //서버로 부터의 응답 가져오기
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+               while(true){ String line = br.readLine();
+                if(line == null){
+                    break;
+                }
+                sb.append(line + "\n");
+
+            }
+             br.close();
+            con.disconnect();
+            //JSON 파싱
+            JSONObject object = new JSONObject(sb.toString());
+            boolean result = object.getBoolean("result");
+            //핸들러에게 결과 전송
+            Message message = new Message();
+            message.obj = result;
+            inserHandler.sendMessage(message);
+
+        }catch(Exception e){
+                Log.e("업로드 예외", e.getMessage());
+            }
+
+
+        }
+    }
+
+    Handler inserHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message message) {
+
+            boolean result = (Boolean) message.obj;
+            if (result == true) {
+                Toast.makeText(InsertActivity.this, "삽입성공", Toast.LENGTH_SHORT).show();
+                //키보드 내리기
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(shopnameinput.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(addressinput.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(roadaddressinput.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(mobileinput.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(businesshourinput.getWindowToken(), 0);
+
+
+            } else {
+                Toast.makeText(InsertActivity.this, "삽입실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+        };
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_insert);
+
+            shopnameinput = (EditText) findViewById(R.id.shopnameinput);
+            addressinput = (EditText) findViewById(R.id.addressinput);
+            businesshourinput = (EditText) findViewById(R.id.businesshourinput);
+            roadaddressinput = (EditText) findViewById(R.id.roadaddressinput);
+            mobileinput = (EditText) findViewById(R.id.mobileinput);
+            btninsert = (Button) findViewById(R.id.btninsert);
+
+
+            btninsert.setOnClickListener(new Button.OnClickListener() {
+
+                public void onClick(View view) {
+                    //유효성 검사
+                    if (shopnameinput.getText().toString().trim().length() < 1) {
+                        Toast.makeText(InsertActivity.this, "이름은 필수 입력입니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (addressinput.getText().toString().trim().length() < 1) {
+                        Toast.makeText(InsertActivity.this, "주소는 필수 입력입니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (roadaddressinput.getText().toString().trim().length() < 1) {
+                        Toast.makeText(InsertActivity.this, "도로명 주소 필수 입력입니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (businesshourinput.getText().toString().trim().length() < 1) {
+                        Toast.makeText(InsertActivity.this, "영업시은 필수 입력입니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (mobileinput.getText().toString().trim().length() < 1) {
+                        Toast.makeText(InsertActivity.this, "전화번호는 필수 입력입니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    new InsertThread().start();
+                }
+            });
+        }
+    }
---
+++ src/main/java/com/example/portfolio/ShopDetailActivity.java
@@ -0,0 +1,162 @@
+package com.example.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShopDetailActivity extends AppCompatActivity {
+//화면에 사용할 뷰
+    TextView lblshopname, lblshopphonenum, lbladdress, lblroadaddress, lblhour;
+
+    //텍스트 데이터를 웹에서 다운로드 받아서 출력
+    //다운로드 > 파싱 > 출력
+
+    //텍스트 데이터를 출력할 핸들러
+    Handler textHandler = new Handler(Looper.getMainLooper()){
+     @Override
+     public void handleMessage(Message message) {
+       //넘어온 데이터 찾아오기
+       Map<String,Object> map = (Map<String, Object>)message.obj;
+       lblshopname.setText((String)map.get("shopname"));
+       lbladdress.setText((String)map.get("address"));
+       lblhour.setText((String)map.get("businesshour"));
+       lblroadaddress.setText((String)map.get("roadaddress"));
+       lblshopphonenum.setText((String)map.get("mobile"));
+       //이미지 파일명을 스레드 에게 넘겨서 출력
+         //new ImageThread((String)map.get("pictureurl"
+     }
+     };
+    //텍스트 데이터를 가져올 스레드 클래스
+    class TextThread extends Thread{
+        StringBuilder sb = new StringBuilder();
+        @Override
+        public void run(){
+            try{
+                URL url = new URL("http://192.168.0.45:8080/portfolio/detail?shopid=" + 1);
+                        //Connetion 객체 만들기
+                        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                //옵션설정
+                con.setUseCaches(false);
+                con.setConnectTimeout(30000);
+
+                //스트림 객체 생성
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                //문자열 읽기
+                while(true){
+                    String line = br.readLine();
+                    if(line == null){
+                        break;
+                    }
+                    sb.append(line + "\n");
+                }
+
+                br.close();
+                con.disconnect();
+            }catch(Exception e){
+                //이 메시지가 보이 서버가 구동 중인지 확인하고 URL은 제대로 입력했는지 확인
+                Log.e("다운로드 에러", e.getMessage());
+            }
+         Log.e("다운로드 받은 문자열", sb.toString());
+
+            try{
+                JSONObject object = new JSONObject(sb.toString());
+                JSONObject shop = object.getJSONObject("shop");
+                String shopname = shop.getString("shopname");
+                String businesshour = shop.getString("businesshour");
+                String mobile = shop.getString("mobile");
+                String roadaddress = shop.getString("roadaddress");
+                String address = shop.getString("address");
+
+                //5개의 데이터 하나로 묶기
+                Map<String, Object> map = new HashMap<>();
+                map.put("shopname", shopname);
+                map.put("businesshour", businesshour);
+                map.put("mobile", mobile);
+                map.put("roadaddress", roadaddress);
+                map.put("address", address);
+
+                //핸들러에게 데이터를 전송하고 호출
+                Message message = new Message();
+                message.obj = map;
+                textHandler.sendMessage(message);
+            }catch(Exception e){
+                Log.e("파싱 에러", e.getMessage());
+            }
+        }
+    }
+    Handler imageHandler = new Handler(Looper.getMainLooper()){
+    @Override
+        public void handleMessage(Message message){
+        //스레드가 전달해준 데이터를 이미지 뷰에 출력
+        Bitmap bitmap = (Bitmap)message.obj;
+          // imgpictureurl.setImageBitmap(bitmap);
+
+    }
+
+    };
+     //이미지 다운로드를 위한 스레드
+    class ImageThread extends Thread{
+        //다운로드 받을 팔일명
+         String pictureurl;
+         public ImageThread(String pictureurl){
+             this.pictureurl = pictureurl;
+         }
+         @Override
+         public void run (){
+           try{
+               URL url = new URL("http://");
+               HttpURLConnection con = (HttpURLConnection)url.openConnection();
+               con.setUseCaches(false);
+               con.setConnectTimeout(20000);
+
+               //바로출력
+               InputStream is = url.openStream();
+               Bitmap bitmap = BitmapFactory.decodeStream(is);
+               //Message저장
+               Message message = new Message();
+               message.obj = bitmap;
+               imageHandler.sendMessage(message);
+           }catch(Exception e){
+               Log.e("이미지 다운 실패", e.getMessage());
+           }
+         }
+     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        new TextThread().start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_detail);
+
+        lblshopname = (TextView)findViewById(R.id.lblshopname);
+        lblshopphonenum = (TextView)findViewById(R.id.lblshopphonenum);
+        lblshopname = (TextView)findViewById(R.id.lblshopname);
+        lbladdress = (TextView)findViewById(R.id.lbladdress);
+        lblroadaddress = (TextView)findViewById(R.id.lblroadaddress);
+        lblhour = (TextView)findViewById(R.id.lblhour);
+
+    }
+}
